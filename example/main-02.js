1f1f143de55c9086fc18908f81769795d11e37c9
I-Lun Chen <newcomer521@gmail.com>
2014-05-23 14:11:23 +0800

    Update main-02.js

--- example/main-02.js
+++ example/main-02.js
@@ -3,7 +3,7 @@ require.config({
 	paths: {
 		'angular': 'vendor/angular/angular',
 		'sdiv': 'directive/sdiv',
-		'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
+		'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min'
 	},
 	shim: {
 		'sdiv': {
